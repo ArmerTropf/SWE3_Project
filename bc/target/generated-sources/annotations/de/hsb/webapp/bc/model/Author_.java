39bbaf6ca0259b5375061e2eb661b001ec060c12
ArmerTropf <mguenster@web.de>
2017-02-21 13:51:42 +0100

    normal

---
+++ bc/target/generated-sources/annotations/de/hsb/webapp/bc/model/Author_.java
@@ -0,0 +1,16 @@
+package de.hsb.webapp.bc.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Author.class)
+public abstract class Author_ {
+
+	public static volatile SingularAttribute<Author, String> firstname;
+	public static volatile SingularAttribute<Author, Integer> aid;
+	public static volatile SingularAttribute<Author, String> lastname;
+
+}
+
